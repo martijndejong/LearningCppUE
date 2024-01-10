1563e2c124a97debcb00856380a9f3c10914ad3d
martijndejong <martijndejong@users.noreply.github.com>
2024-01-10 04:50:34 +0100

    Lecture 2 - until binding input

--- Source/LearningCppUE/Private/SCharacter.cpp
+++ Source/LearningCppUE/Private/SCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "SCharacter.h"
+// include SpringArmComponent and CameraComponent, find location by CTRL+click the <U...Component> after "CreateDefaultSubobject"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -9,6 +12,13 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// MDJ: Create instance of SpringArmComponent and assign to pointer
+	// MDJ: Make SpringArmComp child of RootComponent (like attaching it to the root in editor)
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +26,12 @@ void ASCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+// MDJ: Create MoveForward function to be passed to BindAxis
+void ASCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
 }
 
 // Called every frame
@@ -30,5 +46,11 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// MDJ: Add BindAxis to PlayerInputComponent -- (1 axis name, 2 which character (self), 3 function to execute)
+	// MDJ: name must be same as name added in Editor Input mappings
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+
+	// MDJ: Same as above, but now for turn -- this time use &APawn because that already has function "AddControllerPitchInput"
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerPitchInput);
 }
 
--- Source/LearningCppUE/Public/SCharacter.h
+++ Source/LearningCppUE/Public/SCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "SCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class LEARNINGCPPUE_API ASCharacter : public ACharacter
 {
@@ -16,8 +19,18 @@ public:
 	ASCharacter();
 
 protected:
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	// MDJ: Must declare void MoveForward function in .h before can be created in .cpp
+	void MoveForward(float Value);
 
 public:	
 	// Called every frame
